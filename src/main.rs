9ef62f9065a1497092a8f4f58fd1570f0cb619d5
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2022-12-20 19:00:31 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
-fn main() {
-    todo!();
+use chrono::{DateTime, Utc};
+use dotenvy::dotenv;
+use reqwest::{
+    header::{HeaderMap, AUTHORIZATION},
+    Result,
+};
+use serde::{Deserialize, Serialize};
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().expect(".env file not found.");
+    let deepl_api_key = env::var("DEEPL_API_KEY").expect("DEEPL_API_KEY not found in your ENV.");
+
+    let args: Vec<String> = env::args().collect();
+    let text_to_translate = args[1].as_str();
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        AUTHORIZATION,
+        format!("DeepL-Auth-Key {}", deepl_api_key).parse().unwrap(),
+    );
+
+    let client = reqwest::Client::new();
+
+    let create_glossary = client
+        .post("https://api.deepl.com/v2/glossaries")
+        .headers(headers.clone())
+        .form(&[
+            ("name", "Tmp"),
+            ("source_lang", "JA"),
+            ("target_lang", "EN"),
+            ("entries_format", "csv"),
+            ("entries", "ほげ,hoge"),
+        ])
+        .send()
+        .await?
+        .json::<CreateGlossary>()
+        .await?;
+
+    let glossary_id = create_glossary.glossary_id;
+
+    let translate = client
+        .post("https://api.deepl.com/v2/translate")
+        .headers(headers.clone())
+        .form(&[
+            ("text", text_to_translate),
+            ("source_lang", "JA"),
+            ("target_lang", "EN"),
+            ("glossary_id", &glossary_id),
+        ])
+        .send()
+        .await?
+        .json::<Translations>()
+        .await?;
+
+    println!("{:#?}", &translate);
+
+    // 単語リストは既存のものを編集してくれないので、毎回後処理で消す
+    client
+        .delete(format!(
+            "https://api.deepl.com/v2/glossaries/{}",
+            &glossary_id
+        ))
+        .headers(headers)
+        .send()
+        .await?;
+
+    Ok(())
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CreateGlossary {
+    glossary_id: String,
+    ready: bool,
+    name: String,
+    source_lang: String,
+    target_lang: String,
+    creation_time: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Translations {
+    translations: Vec<Translation>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Translation {
+    text: String,
 }
